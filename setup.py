58628bb57b8dd5cadefd1c1b2c175352aa309d2a
Raphaël Vinot <raphael@vinot.info>
2014-02-24 18:33:32 +0100

    Update installer

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from distutils.core import setup
 
 setup(
     name='urlquery',
-    version='0.1',
-    description='API to access urlquery.',
+    version='2.0',
+    description='Python API to access urlquery v2',
     url='https://github.com/CIRCL/urlquery-py.git',
     author='Raphaël Vinot',
     author_email='raphael.vinot@circl.lu',
