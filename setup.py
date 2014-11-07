0bd2a0b6e040a51b6311d390d542fec3e6679a26
Raphaël Vinot <raphael@vinot.info>
2014-11-07 11:29:08 +0100

    PEP8

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from distutils.core import setup
 
 setup(
     name='urlquery',
-    version='2.0',
+    version='2.0.1',
     description='Python API to access urlquery v2',
-    url='https://github.com/CIRCL/urlquery-py.git',
+    url='https://github.com/CIRCL/urlquery-py',
     author='Raphaël Vinot',
     author_email='raphael.vinot@circl.lu',
     maintainer='Raphaël Vinot',
     maintainer_email='raphael.vinot@circl.lu',
     packages=['urlquery'],
     license='GNU GPLv3',
-    long_description=open('README.md').read(),
-    )
+    install_requires=['requests', 'python-dateutil'],
+)
