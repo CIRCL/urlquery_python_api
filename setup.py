b36928d47d6d0c6f5b6bd6a8aad8678db0a9ef41
Raphaël Vinot <raphael@vinot.info>
2015-04-30 16:31:00 +0200

    Update version to 2.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='urlquery',
-    version='2.0.1',
+    version='2.1',
     description='Python API to access urlquery v2',
     url='https://github.com/CIRCL/urlquery-py',
     author='Raphaël Vinot',
@@ -12,6 +12,16 @@ setup(
     maintainer='Raphaël Vinot',
     maintainer_email='raphael.vinot@circl.lu',
     packages=['urlquery'],
-    license='GNU GPLv3',
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Telecommunications Industry',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Security',
+        'Topic :: Internet',
+    ],
     install_requires=['requests', 'python-dateutil'],
 )
